969c70b2668c28932c21a2c35197d0bfc6fa5847
songxinjianqwe <songxinjianzx@163.com>
2018-07-14 15:17:39 +0800

    抽象Registry层

--- toy-rpc-core/src/main/java/com/sinjinsong/toy/registry/ZookeeperConstant.java
+++
@@ -1,9 +0,0 @@
-package com.sinjinsong.toy.registry;
-
-
-public class ZookeeperConstant {
-    public static final int ZK_SESSION_TIMEOUT = 5000;
-    public static final String ZK_REGISTRY_PATH = "/toy";
-    
-    
-}
